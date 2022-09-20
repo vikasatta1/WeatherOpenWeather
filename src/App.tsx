08b8e32d0f7fd310ad0726b9c1e817de8782ca44
Vika <Vikasatta1998@gmail.com>
2022-09-20 22:01:57 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ChangeEvent, useEffect, useState} from 'react';
+import axios from 'axios'
+import './index.css';
+type dataResponce = {
+    base:string,
+    clouds: {all: number},
+    cod: number,
+    coord: {lon: number, lat: number},
+    dt:number,
+    id: number,
+    main: any,
+    name:string,
+    sys: {type: number, id: number, country: string, sunrise: number, sunset: number},
+    timezone: number,
+    visibility:number,
+    weather:Array<any>
+    wind: {speed: number, deg: number}
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [cityName, setCityName] = useState('')
+    const [data, setData] = useState<dataResponce | null>(null)
+    const [location, setLocation] = useState('')
+    const apiKey = '24cd562bc08de33b11c7e4e4c48459bd'
+
+    const changeInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setLocation(e.currentTarget.value)
+    }
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`
+    const searchLocation = (event: any) => {
+        if (event.key === "Enter") {
+
+            axios.get(url).then(res => {
+                setData(res.data)
+                console.log(res.data)
+                setLocation('')
+            })
+        }
+    }
+
+let celcia = (+(data?.main.feels_like) - 32) / 1.8000
+
+    return (
+        <div className="app">
+            <div className="search">
+                <input
+                    type={'text'}
+                    placeholder={'Enter Location'}
+                    value={location} onChange={changeInput}
+                onKeyPress={searchLocation}
+                />
+            </div>
+            <div className="container">
+
+                <div className="top">
+                    <div className="location">
+                        {data ? <p>{data?.name}</p> : ''}
+                    </div>
+                    <div className="temp">
+                        <h1>{celcia}°C</h1>
+                    </div>
+                    <div className="description">
+                        <p>Clouds</p>
+                    </div>
+                </div>
+                <div className="bottom">
+                    <div className="feels">
+                        <p>{celcia}°C</p>
+                        <p>Feels Like</p>
+                    </div>
+                    <div className="humidity">
+                        <p>{data?.main.humidity}%</p>
+                        <p>Humidity</p>
+                    </div>
+                    <div className="wind">
+                        <p>{data?.wind.speed} MPH</p>
+                        <p>Winds</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
