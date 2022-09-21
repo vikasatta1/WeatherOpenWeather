8b96695b8d013ab227cca7f61e37911bfff8b0e3
Vika <Vikasatta1998@gmail.com>
2022-09-21 21:32:40 +0300

    delete console.log

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,6 @@ function App() {
 
             axios.get(url).then(res => {
                 setData(res.data)
-                console.log(res.data)
                 setLocation('')
             })
         }
